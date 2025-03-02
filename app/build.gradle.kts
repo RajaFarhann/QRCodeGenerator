e5560255b3082f7f8ac72fe8e86b49660414e7e7
Raja Muhammad Farhan Zahputra <raja.farhan0503@gmail.com>
2025-03-02 14:16:10 +0700

    Add library  QR in build.gradle.kts and Add read and write permission in androidmanifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.rajafarhan.qrcodegenerator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rajafarhan.qrcodegenerator"
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //QR Code
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
